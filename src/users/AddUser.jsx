166f9748ba30a06ea1817ef91ec76a782a7dd002
Kelvin Irungu <kelvinn.irungu69@gmail.com>
2024-03-01 18:20:28 +0300

    add user structure

--- src/users/AddUser.jsx
+++ src/users/AddUser.jsx
@@ -2,6 +2,41 @@ import React from 'react'
 
 export const AddUser = () => {
   return (
-    <div>AddUser</div>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
+          <h2>Register</h2>
+          <div className='mb-3'>
+            <label htmlFor="Name">Name</label>
+            <input 
+            type="text"
+            className='form-control'
+            placeholder='Enter your name'
+            name='name'
+             />
+          </div>
+          <div className='mb-3'>
+            <label htmlFor="Username">Username</label>
+            <input 
+            type="text"
+            className='form-control'
+            placeholder='Enter your username'
+            username='username'
+             />
+          </div>
+          <div className='mb-3'>
+            <label htmlFor="Email">Email</label>
+            <input 
+            type="text"
+            className='form-control'
+            placeholder='Enter your email'
+            email='email'
+             />
+          </div>
+          <button type="button" className="btn btn-outline-success mx-3">Submit</button>
+          <button type="button" className="btn btn-outline-danger mx-3">Cancel</button>
+        </div>
+      </div>
+    </div>
   )
 }
