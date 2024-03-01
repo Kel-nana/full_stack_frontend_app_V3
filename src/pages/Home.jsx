a19bbf77ac6982f077c4242c8ab2b4e299dc72bc
Kelvin Irungu <kelvinn.irungu69@gmail.com>
2024-03-01 15:11:39 +0300

    Add button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,6 +26,11 @@ export const Home = () => {
             <td>{user.name}</td>
             <td>{user.username}</td>
             <td>{user.email}</td>
+            <td>
+            <button type="button" className="btn btn-primary">View</button>
+            <button type="button" className="btn btn-outline-info">Edit</button>
+            <button type="button" className="btn btn-danger">Delete</button>
+            </td>
         </tr>
     ));
 
@@ -35,10 +40,11 @@ export const Home = () => {
                 <table className="table table-striped table-hover table-dark">
                     <thead>
                         <tr>
-                            <th scope="col">Id</th>
+                            <th scope="col">No.</th>
                             <th scope="col">Name</th>
                             <th scope="col">Username</th>
                             <th scope="col">Email</th>
+                            <th scope="col">Action</th>
                         </tr>
                     </thead>
                     <tbody>
