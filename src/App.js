c1ae43a96d544da1b948c1e5bea23716da34543f
Kelvin Irungu <kelvinn.irungu69@gmail.com>
2024-03-01 17:57:19 +0300

    add router and paths

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import { Navbar } from './layout/Navbar';
 import { Home } from './pages/Home';
+import { AddUser} from './users/AddUser'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+        <div className="App">
       <Navbar />
      <h2>Let's get started</h2>
-     <Home />
-    </div>
+     <Routes>
+        <Route exact path ="/" element={<Home />}/>
+        <Route exact path ="/AddUser" element={<AddUser />}/>
+     </Routes>
+        </div>
+      </Router>
   );
 }
 
