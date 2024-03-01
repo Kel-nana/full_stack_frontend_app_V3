0a53972e6be767dc6aa70f2c9c39c81813e0ba52
Kelvin Irungu <kelvinn.irungu69@gmail.com>
2024-03-01 03:40:03 +0300

    add navbar

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from  './navbar.module.css';
+import logoImg from '../assets/logo_sample.jpeg';
+
+export const Navbar = () => {
+  return (
+    <div>
+        <nav className="navbar bg-body-tertiary">
+  <div className={`container ${classes.logo_img}`}>
+    <a className="navbar-brand" href="#">
+      <img src={logoImg} alt="logo" width="50" height="44" />
+    </a>
+  </div>
+</nav>
+    </div>
+  )
+}
+
