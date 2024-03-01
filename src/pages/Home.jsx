7ad95fcda08f5c590730841301365612c6027613
Kelvin Irungu <kelvinn.irungu69@gmail.com>
2024-03-01 14:23:37 +0300

    cleaning up code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,62 +5,47 @@ import classes from './home.module.css';
 export const Home = () => {
     const [users, setUsers] = useState([]);
 
-    useEffect(()=>{
-        const loadUsers =async ()=> {
-            let result = await axios.get("http://localhost:8080/users");
-            result= result.data
-            setUsers(result)
-        }
-loadUsers()
-    }, [])
+    useEffect(() => {
+        const loadUsers = async () => {
+            try {
+                const result = await axios.get("http://localhost:8080/users");
+                setUsers(result.data);
+            } catch (error) {
+                console.error("Error fetching users:", error);
+            }
+        };
 
-    console.log(users, 'DATA FETCHED')
-    // const userData = users.map((user, index)=> {
-    //   <tr key={user.id}>
-    //   <th scope="row">{index}</th>
-    //   <td>{user.name}</td>
-    //   <td>{user.username}</td>
-    //   <td>{user.email}</td>
-    // </tr>
-    // });
+        loadUsers();
+    }, []);
+
+    console.log(users, 'DATA FETCHED');
+
     const userData = users.map((user, index) => (
-      <tr key={user.id}>
-          <th scope="row">{index}</th>
-          <td>{user.name}</td>
-          <td>{user.username}</td>
-          <td>{user.email}</td>
-      </tr>
-  ));
+        <tr key={user.id}>
+            <th scope="row">{index}</th>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>{user.email}</td>
+        </tr>
+    ));
 
-
-  return (
-    <div className={`container ${classes.home_table}`}>
-        <div className='py-4'>
-        <table className="table table-striped table-hover table-dark">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Name</th>
-      <th scope="col">Username</th>
-      <th scope="col">Email</th>
-    </tr>
-  </thead>
-  <tbody>
-    {userData}
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+    return (
+        <div className={`container ${classes.home_table}`}>
+            <div className='py-4'>
+                <table className="table table-striped table-hover table-dark">
+                    <thead>
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {userData}
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
-  )
+    );
 }
