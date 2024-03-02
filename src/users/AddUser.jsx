b977a23d5a0dab273bbde1242bff4cff2eeb8014
Kelvin Irungu <kelvinn.irungu69@gmail.com>
2024-03-02 17:50:05 +0300

    navigate to hommepage & submit handler

--- src/users/AddUser.jsx
+++ src/users/AddUser.jsx
@@ -1,7 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import { InputField } from '../UI/InputField'
+import { Link, useNavigate } from 'react-router-dom';
 
 export const AddUser = () => {
+  let navigate = useNavigate();
   const [user, setUser] = useState({
     name: "",
     username: "",
@@ -13,12 +16,18 @@ console.log(user, "user input data")
   const onInputChange = (e)=> {
 setUser({...user,[e.target.name]: e.target.value});
   }
+
+  const onSubmitHandler = async (e)=> {
+    e.preventDefault();
+    await axios.post("http://localhost:8080/user", user);
+    navigate("/")
+  };
   return (
     <div className='container'>
       <div className='row'>
         <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
-          <h2>Register</h2>
-          <form>
+          <h2>Add User</h2>
+          <form onSubmit={onSubmitHandler}>
           <InputField
               label="Name"
               type="text"
@@ -44,8 +53,8 @@ setUser({...user,[e.target.name]: e.target.value});
               onChange={onInputChange}
             />
 
-            <button type="button" className="btn btn-outline-success mx-3" >Submit</button>
-            <button type="button" className="btn btn-outline-danger mx-3">Cancel</button>
+            <button type="submit" className="btn btn-outline-success mx-3" >Submit</button>
+            <Link to="/" className="btn btn-outline-danger mx-3">Cancel</Link>
           </form>
         </div>
       </div>
