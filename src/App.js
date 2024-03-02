b7e7913fda09000d0d69959529fcb523d514a58a
Kelvin Irungu <kelvinn.irungu69@gmail.com>
2024-03-02 17:51:05 +0300

    add route for edit user

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './layout/Navbar';
 import { Home } from './pages/Home';
-import { AddUser} from './users/AddUser'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AddUser} from './users/AddUser';
+import { EditUser } from './users/EditUser';
+
 
 function App() {
   return (
-    <Router>
         <div className="App">
+    <Router>
       <Navbar />
      <h2>Let's get started</h2>
      <Routes>
         <Route exact path ="/" element={<Home />}/>
-        <Route exact path ="/AddUser" element={<AddUser />}/>
+        <Route exact path ="/addUser" element={<AddUser />}/>
+        <Route exact path ="/editUser/:id" element={<EditUser />}/>
      </Routes>
-        </div>
       </Router>
+        </div>
   );
 }
 
