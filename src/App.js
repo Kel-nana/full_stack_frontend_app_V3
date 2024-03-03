506deaf6e2d71184214582c15417a794c6c0e4c9
Kelvin Irungu <kelvinn.irungu69@gmail.com>
2024-03-03 17:15:35 +0300

    toggle state management

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -5,21 +6,28 @@ import { Navbar } from './layout/Navbar';
 import { Home } from './pages/Home';
 import { AddUser} from './users/AddUser';
 import { EditUser } from './users/EditUser';
-
+import { ToggleButton } from './UI/ToggleButton';
 
 function App() {
+  const [toggle, setToggle] = useState(true);
+console.log(toggle, 'toggle')
+  const toggleLightMode = () => {
+    setToggle(!toggle);
+  };
+
   return (
-        <div className="App">
-    <Router>
-      <Navbar />
-     <h2>Let's get started</h2>
-     <Routes>
-        <Route exact path ="/" element={<Home />}/>
-        <Route exact path ="/addUser" element={<AddUser />}/>
-        <Route exact path ="/editUser/:id" element={<EditUser />}/>
-     </Routes>
+    <div className={`App ${toggle ? 'toggle text-dark' : ''}`}>
+      <Router>
+        <Navbar />
+        <ToggleButton animation={toggle} toggleLightMode={toggleLightMode} />
+        <h2>Let's get started</h2>
+        <Routes>
+          <Route exact path="/" element={<Home text_color={toggle}/>} />
+          <Route exact path="/addUser" element={<AddUser />} />
+          <Route exact path="/editUser/:id" element={<EditUser />} />
+        </Routes>
       </Router>
-        </div>
+    </div>
   );
 }
 
