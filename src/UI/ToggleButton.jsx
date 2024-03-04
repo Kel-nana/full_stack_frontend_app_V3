d0314d405c77547967ef4f2d8802c56c98d0b543
Kelvin Irungu <kelvinn.irungu69@gmail.com>
2024-03-04 23:10:38 +0300

    background image toggle conditional

--- src/UI/ToggleButton.jsx
+++ src/UI/ToggleButton.jsx
@@ -6,7 +6,7 @@ export const ToggleButton = ({ animation, toggleLightMode }) => {
   };
  console.log(animation, 'animation')
   return (
-    <div className=" body_toggle" onClick={handleToggle} >
+    <div className={`body_toggle ${animation ? 'toggle_bg_img' : 'toggle_bg_img_moon'}`} onClick={handleToggle} >
       <div >
       <span className={`moon ${animation ? '' : 'animate_left'}`}/>
       </div>
