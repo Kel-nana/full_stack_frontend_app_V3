f38091351eff2dc6000ac1feccc31598b4b216e9
Kelvin Irungu <kelvinn.irungu69@gmail.com>
2024-03-05 17:56:06 +0300

    use location to get path

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -3,12 +3,15 @@ import {Link} from 'react-router-dom'
 import classes from  './navbar.module.css';
 import logoImg from '../assets/logo_sample.jpeg';
 
-export const Navbar = () => {
+export const Navbar = ({nav_mode, setDataNav}) => {
+ const navDataFunction = ()=> {
+  setDataNav(null);
+ }
   return (
     <div>
-        <nav className="navbar bg-body-tertiary">
+        <nav className={` navbar bg-body-tertiary ${nav_mode ? 'nav_toggle' : ''}`}>
   <div className={`container ${classes.logo_img}`}>
-  <Link to="/" className="navbar-brand">
+  <Link to="/" className="navbar-brand" onClick={navDataFunction}>
             <img src={logoImg} alt="logo" width="50" height="44" />
           </Link>
     <Link type="button" className="btn btn-outline-secondary" to="/addUser">Add User</Link>
